4b23d310ac6e8c1c1c321bec49cbc0602f1de93e
DuocAnh <duocanh14@gmail.com>
2024-11-04 23:58:10 +0700

    finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
+import Button from './UI/Button'
 import logoImg from '../assets/logo.jpg'
+import { useContext } from 'react'
+import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  function hadleShowCart() {
+    userProgressCtx.showCart();
+  }
+
   return (
     <header id="main-header">
       <div id="title">
@@ -10,7 +25,9 @@ export default function Header() {
       <p>
       </p>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly onClick={hadleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   )
